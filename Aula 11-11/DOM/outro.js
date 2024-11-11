2e73eb9821a74b5395a72fb332e24948cdbc4a6c
tiagoprog <162224165+tiagoprog@users.noreply.github.com>
2024-11-11 15:14:36 -0300

    Add files via upload

---
+++ Aula 11-11/DOM/outro.js
@@ -0,0 +1,49 @@
+const params = new URLSearchParams(window.location.search);
+
+const id = params.get('id')
+
+const pega_json = async (caminho) => {
+    const resposta = await fetch(caminho);
+    const dados = await resposta.json();
+    return dados;
+}
+
+const acha_cookie = (chave) => {
+    const array_cookie = document.cookie.split('; ');
+    const procurado = array_cookie.find(
+        ( ele ) => ele.startsWith(`${chave}=`)
+    )
+
+    return procurado.split('=')[1];
+}
+
+
+console.log(acha_cookie('nome'));
+
+console.log(sessionStorage.getItem('nome'));
+
+const obj = JSON.parse(sessionStorage.getItem('atleta'));
+
+console.log(obj.caminhoImagem);
+
+if (sessionStorage.getItem('logado')){
+
+    pega_json(`https://botafogo-atletas.mange.li/2024-1/${id}`).then(
+        ( atleta ) => {
+            document.body.innerHTML = '';
+        
+            const nome = document.createElement('h1');
+            nome.innerHTML = atleta.nome;
+
+            document.body.appendChild(nome);
+
+            const imagem = document.createElement('img');
+            imagem.src = atleta.imagem;
+            imagem.alt = `foto de ${atleta.nome}`
+
+            document.body.appendChild(imagem);
+        }
+    )
+} else {
+    document.body.innerHTML = '<h1>É preciso estar logado para ver a página.</h1>'
+}
